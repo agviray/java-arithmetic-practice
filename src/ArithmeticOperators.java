6cf24dea46caf3d8d34d6d89464e7ebc339c54e2
Allan Viray <allang.v.dev@gmail.com>
2023-09-23 20:42:27 -0500

    First commit

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperators
+{
+    public static void main(String args[])
+    {
+        int a=14, b=5;
+        int c=a/b;
+        float cAsFloatDataType=(float)a/b;
+        int r=a%b;
+
+        System.out.println(c);
+        System.out.println(r);
+        System.out.println(cAsFloatDataType);
+    }
+}
