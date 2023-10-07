9bc66232037c5de116bfb55eb0f8fe877fade740
Allan Viray <allang.v.dev@gmail.com>
2023-10-07 13:00:23 -0500

    Set up initial AreaAndVolumeOfCuboid content

---
+++ src/AreaAndVolumeOfCuboid.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Scanner;
+
+// - Write a program to find the total area and volume of a given cuboid.
+public class AreaAndVolumeOfCuboid {
+    public static void main(String[] args) {
+        int length, width, height;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
+    }
+}
