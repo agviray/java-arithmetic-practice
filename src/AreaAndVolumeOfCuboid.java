cad933e30cd37650673263ef329ffdd89f046163
Allan Viray <allang.v.dev@gmail.com>
2023-10-07 13:06:33 -0500

    Print directions in console and allow user to define values for length, width, and height variables

--- src/AreaAndVolumeOfCuboid.java
+++ src/AreaAndVolumeOfCuboid.java
@@ -6,6 +6,13 @@ public class AreaAndVolumeOfCuboid {
     public static void main(String[] args) {
         int length, width, height;
         Scanner sc = new Scanner(System.in);
-        System.out.println("hello");
+
+        System.out.println("To find the total area and volume of a cuboid, we'll first enter values for the cuboid's length, width, and height.");
+        System.out.println("Enter a length value.");
+        length=sc.nextInt();
+        System.out.println("Enter a width value.");
+        width=sc.nextInt();
+        System.out.println("Enter a height value.");
+        height=sc.nextInt();
     }
 }
